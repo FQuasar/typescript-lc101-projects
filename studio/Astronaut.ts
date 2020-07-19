3b0b2fb43eb90da0c1c211d23625caf791d3e44a
= <=>
2020-07-19 10:12:41 -0500

    last commit

--- studio/Astronaut.ts
+++ studio/Astronaut.ts
@@ -1,3 +1,10 @@
-export class Astronaut {
-    
+import {Payload} from "./Payload";
+
+export class Astronaut implements Payload {
+     massKg: number;
+     name: string;
+     constructor(massKg: number, name: string){
+         this.massKg = massKg;
+         this.name = name;
+     } 
 }
